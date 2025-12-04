0db5bdd8055cdce11ecf51ff18ed14a06453a061
Andre Luis da Cunha <andrelcunha81@gmail.com>
2025-12-04 10:50:35 -0300

    ✨ feat: implement GetCommand with validation and execution logic

---
+++ internal/core/command/get.go
@@ -0,0 +1,55 @@
+package command
+
+import (
+	"github.com/andrelcunha/goodiesdb/internal/core/store"
+	"github.com/andrelcunha/goodiesdb/internal/protocol"
+)
+
+type GetCommand struct{}
+
+func NewGetCommand() *GetCommand {
+	return &GetCommand{}
+}
+
+func (c *GetCommand) Name() string {
+	return "GET"
+}
+
+func (c *GetCommand) MinArgs() int {
+	return 1
+}
+
+func (c *GetCommand) MaxArgs() int {
+	return 1
+}
+
+func (c *GetCommand) RequiresAuth() bool {
+	return false // TODO: refactor authentication
+}
+
+func (c *GetCommand) Validate(args []string) error {
+	if len(args) != 1 {
+		return ErrWrongNumberOfArguments
+	}
+	return nil
+}
+
+func (c *GetCommand) Execute(ctx *Context, args []string) (protocol.RESPValue, error) {
+	value, ok := ctx.Store.Get(ctx.DBIndex, args[0])
+	if !ok {
+		return ctx.Store.Protocol.EncodeNil(), nil
+	}
+
+	if value.IsExpired() {
+		// lazy delete
+		ctx.Store.Delete(ctx.DBIndex, args[0])
+		return ctx.Store.Protocol.EncodeNil(), nil
+	}
+
+	str, err := value.ToString()
+	if err != nil {
+		return nil, store.ErrWrongType
+	}
+
+	return protocol.BulkString([]byte(str)), nil
+}
