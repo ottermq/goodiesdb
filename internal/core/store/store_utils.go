c57eb3d328f5a747638fa115b60f5505b32afb97
Andre Luis da Cunha <andrelcunha81@gmail.com>
2025-12-04 10:50:48 -0300

    ✨ feat: add Delete method to remove keys from the store

--- internal/core/store/store_utils.go
+++ internal/core/store/store_utils.go
@@ -1,7 +1,15 @@
 package store
 
-// delKey deletes a key from the store and its expiration
+// Delele a key from the store
+func (s *Store) Delete(dbIndex int, key string) {
+	s.delKey(dbIndex, key)
+}
+
+// delKey deletes a key from the store and its expiration (protected)
 func (s *Store) delKey(dbIndex int, key string) {
+	// mu
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	delete(s.data[dbIndex], key)
 }
 
